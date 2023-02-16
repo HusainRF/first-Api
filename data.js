8b415f9c54703916758589ba3d5d6e7cd17bc85b
HusainRF <justgetupanddoit9@gmail.com>
2023-02-16 20:45:23 +0530

    data.js file updated and multiple user data export in the form of array and data is int the form of json

--- data.js
+++ data.js
@@ -1,2 +1,8 @@
-const data = {name:'Husain' , email:'husain@test.com'};
+// data of multiple users then we have to create the array
+
+const data = [
+    {name:'Husain' , email:'husain@test.com',wheight:'95kg'},
+    {name:'Hatim' , email:'husain@test.com' , age:'20'},
+    {name:'Ram' , email:'ram@test.com',contact:'999'}
+];
 module.exports = data;
