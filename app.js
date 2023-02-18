d53651caac98169241aa210a8f9d68836ca6f1fe
HusainRF <justgetupanddoit9@gmail.com>
2023-02-18 15:24:52 +0530

    Promise() and solve the problem of asynchronous lang

--- app.js
+++ app.js
@@ -21,9 +21,21 @@
 var a = 10 ;
 var b = 0 ;
 
-setTimeout(()=>{
-    b=20;
+// setTimeout(()=>{
+//     b=20;
+// });
+
+// console.log(a + b); // it prints 10 because, before executing b=20 it execute console
+
+//solution:
+
+const waitingData = new Promise((resolve , reject)=>{
+    setTimeout(()=>{
+        resolve(20); // send the value of b
+    },2000);
 });
 
-console.log(a + b); // it prints 10 because, before executing b=20 it execute console
- 
+waitingData.then((data)=>{
+    b =data;
+    console.log(a+b); //this will wait until resolve function is call(resolve func will call after 2sec of excution of node)
+});
