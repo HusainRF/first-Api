8e5e6e78db1babcc0aaaa070abfa6aa2669497fb
HusainRF <justgetupanddoit9@gmail.com>
2023-02-22 18:26:31 +0530

    this is inital commit

--- app.js
+++ app.js
@@ -1,41 +1,8 @@
-// const http = require('http');
-// const data = require('./data.js');
+const express = require("express");
+const app = express();
 
-// http.createServer(function (req,res){
-//     res.writeHead(200,{'Content-Type':'application\json'});
-//     res.write(JSON.stringify(data));
-//     res.end();
-// }).listen(5000);
-
-// part-1
-// console.log("1st to execute");
-
-// setTimeout(()=>{
-//     console.log("2nd to execute "); // it execute 3rd due to delay
-// },2000);
-
-// console.log("3rd to execute"); // but it execute 2nd
-
-
-// drawback of Asynchronous lang
-var a = 10 ;
-var b = 0 ;
-
-// setTimeout(()=>{
-//     b=20;
-// });
-
-// console.log(a + b); // it prints 10 because, before executing b=20 it execute console
-
-//solution:
-
-const waitingData = new Promise((resolve , reject)=>{
-    setTimeout(()=>{
-        resolve(20); // send the value of b
-    },2000);
+app.get("" , (req ,res)=> { 
+    res.send("this is home page");
 });
 
-waitingData.then((data)=>{
-    b =data;
-    console.log(a+b); //this will wait until resolve function is call(resolve func will call after 2sec of excution of node)
-});
+app.listen(5000);
