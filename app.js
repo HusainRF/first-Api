db9aab92a71b3566e9e8ad0847fe888795a369c2
HusainRF <justgetupanddoit9@gmail.com>
2023-02-22 20:55:20 +0530

    add link between multiple pages

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const app = express();
 
-app.get("" , (req ,res)=> { 
+app.get("/" , (req ,res)=> { 
     res.send("this is home page");
+});
+
+app.get("/about" , (req,res)=>{
+    res.send(`
+        <h1>this is about page</h1>
+        <a href="/">go to home</a>
+    `);
 });
 
 app.listen(5000);
