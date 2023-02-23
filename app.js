54f14ed42e047d0ec9404bf864640e2cb1438347
HusainRF <justgetupanddoit9@gmail.com>
2023-02-23 20:36:27 +0530

    learned about how to sendFile() works to send html file on browser

--- app.js
+++ app.js
@@ -4,7 +4,14 @@ const path = require('path');
 const app = express();
 const publicPath = path.join(__dirname , '/public');
 
-//all other page can be accesed by the url loacalhost:5000/about.html
-app.use(express.static(publicPath)); // here we load public folder in our server 
+app.get('' , (_, res)=>{
+    res.sendFile(`${publicPath}/index.html`);
+});
+
+app.get('/about' , (_,res) =>{
+    res.sendFile(`${publicPath}/about.html`);
+});
+
+
 
 app.listen(5000); 
