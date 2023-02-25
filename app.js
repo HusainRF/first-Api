be8ecd38b82fd8d90aab092045f01546ba244e4b
HusainRF <justgetupanddoit9@gmail.com>
2023-02-25 12:52:56 +0530

    learned how to group multiple route using express.Router

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
 const express = require('express')
+
+// for grouping of multiple routes to fall under same  middleware 
+// we have to add a new function router()
+const route = express.Router();
 const reqFilter = require('./middleware');
-// taking middle ware from different file
+
 
 const app = express();
+app.use(reqFilter);
 
 app.get('/' , (req ,res) => {
+    // here we din't apply middleware
     res.send("These is home page");
 });
 
-app.get('/user' , (req, res) =>{
+
+// instead of using app >>> we use "route" object to grp them together
+route.get('/user' , (req, res) =>{
+    // grouping route level middle ware to this route
     res.send("These is user page");
 });
 
-app.get('/about' ,reqFilter ,(req, res) =>{
-    // adding route level middle ware only to these route
+// instead of using app >>> we use "route" object
+route.get('/about' ,(req, res) =>{
+    // grouping route level middle ware to this route
     res.send("These is about page");
 });
+
+// to group middleware we have to do these compulsory
+app.use('/' , route); 
 
 app.listen(5000);
