fb3bb464f2f0f550f2738652b49c0de96693843e
HusainRF <justgetupanddoit9@gmail.com>
2023-02-25 11:54:24 +0530

    added middleware from different file and it works in simliar way as befort

--- app.js
+++ app.js
@@ -1,24 +1,8 @@
 const express = require('express')
+const reqFilter = require('./middleware');
+// taking middle ware from different file
+
 const app = express();
-
-// setting the middleware for filtering the request 
-// coming from client side
-
-const reqFilter = (req, res, next)=>{
-    if(!req.query.age)
-    {
-        res.send("Please enter the age!!");
-    }
-    else if(req.query.age < 18){
-        // basically check for the age of user must be above 18 for using this side 
-        res.send("You cannot access this page...!!");
-    }
-    else 
-        next();
-}
-
-// for route level middle ware we have to comment these 
-// app.use(reqFilter);
 
 app.get('/' , (req ,res) => {
     res.send("These is home page");
