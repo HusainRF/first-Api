bf43c2c8a41ddcf15b5f4d84ff039f745e82b2be
HusainRF <justgetupanddoit9@gmail.com>
2023-03-06 22:40:47 +0530

    learn how to insert multiple documents in nodejs

--- insert.js
+++ insert.js
@@ -2,8 +2,12 @@ const dbConnect = require('./mongodb');
 
 const insert1 = async ()=> {
     const db = await dbConnect();
-    const result = await db.insertOne(
-        {name: 'max1' , brand :'micomax' , price :100 , category:'mobile'}
+    const result = await db.insertMany(
+        [
+            {name: 'max 2' , brand :'micomax' , price :150 , category:'mobile'},
+            {name: 'max 3' , brand :'micomax' , price :200 , category:'mobile'},
+            {name: 'max 4' , brand :'micomax' , price :350 , category:'mobile'}
+        ]
     );
     if(result.acknowledged){
         console.log('data inserted');
