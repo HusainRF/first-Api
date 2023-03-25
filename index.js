6a0ac2644fecbf587ff341e7462dae3d81c4c1df
HusainRF <justgetupanddoit9@gmail.com>
2023-03-25 23:07:10 +0530

    put() and delete() in nodejs to delete data in mongodb using mongoose

--- index.js
+++ index.js
@@ -11,4 +11,16 @@ app.post("/create", async (req, resp)=>{
     resp.send(result);
 });
 
+app.get("/list" , async (req ,resp)=>{
+    let data = await Product.find();
+    resp.send(data);
+})
+
+app.delete("/delete/:_id" , async (req ,resp)=>{
+    console.log(req.params);
+    const data = await Product.deleteOne(req.params);
+    resp.send(data);
+})
+
+
 app.listen(5000);
