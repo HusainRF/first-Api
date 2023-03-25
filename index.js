74a8513b50900b72b755954378b9a2a8a625ba5d
HusainRF <justgetupanddoit9@gmail.com>
2023-03-25 14:37:25 +0530

    CURD opration in mongoose

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
 
-const main = async ()=>{
-    await mongoose.connect("mongodb://127.0.0.1:27017/e-comm");
-    const ProductSchema =  new mongoose.Schema({
-        name : String,
-    });
+mongoose.connect("mongodb://127.0.0.1:27017/e-comm");
+
+const ProductSchema =  new mongoose.Schema({
+    name : String,
+    price : Number,
+    brand: String,
+    category: String
+});
+
+const saveInDB = async ()=>{
     const ProductModel = mongoose.model('products', ProductSchema);
     let data =  new ProductModel({
         name : "samsung s22",
@@ -13,5 +18,29 @@ const main = async ()=>{
     let result = await data.save();
     console.log(result);
 }
+// saveInDB();
 
-main();
+const updateInDB = async ()=>{
+    const ProductModel = mongoose.model('products',ProductSchema);
+    let data =await ProductModel.updateOne(
+        {name:"max 3"},
+        { $set:{name:"max 4" , price: 500} }
+    )
+    console.log(data);
+}
+// updateInDB();
+
+const deleteInDB = async ()=>{
+    const ProductModel = mongoose.model('products' , ProductSchema);
+    let data = await ProductModel.deleteOne({name: 'max 4'});
+    console.log(data);
+}
+// deleteInDB();
+
+const findInDB = async ()=>{
+    const ProductModel = mongoose.model('products' , ProductSchema);
+    let data = await ProductModel.find({name :'max 4'});
+    console.log(data);
+}
+
+findInDB();
