26b6852c31ef112fc27f5d99bbde89f1972c1c25
HusainRF <justgetupanddoit9@gmail.com>
2023-03-25 22:59:51 +0530

    using mongoose created basic api

--- data.js
+++
@@ -1,8 +0,0 @@
-// if data is of multiple users then we have to create the array
-
-const data = [
-    {name:'Husain' , email:'husain@test.com',wheight:'95kg'},
-    {name:'Hatim' , email:'husain@test.com' , age:'20'},
-    {name:'Ram' , email:'ram@test.com',contact:'999'}
-];
-module.exports = data;
--- index.js
+++ index.js
@@ -1,46 +1,14 @@
-const mongoose = require('mongoose');
+const express = require("express");
+require('./config');
+const Product = require('./product');
+const app = express();
 
-mongoose.connect("mongodb://127.0.0.1:27017/e-comm");
+app.use(express.json());
 
-const ProductSchema =  new mongoose.Schema({
-    name : String,
-    price : Number,
-    brand: String,
-    category: String
+app.post("/create", async (req, resp)=>{
+    let data = new Product(req.body);
+    let result = await data.save();
+    resp.send(result);
 });
 
-const saveInDB = async ()=>{
-    const ProductModel = mongoose.model('products', ProductSchema);
-    let data =  new ProductModel({
-        name : "samsung s22",
-        price : 1000
-    });
-    let result = await data.save();
-    console.log(result);
-}
-// saveInDB();
-
-const updateInDB = async ()=>{
-    const ProductModel = mongoose.model('products',ProductSchema);
-    let data =await ProductModel.updateOne(
-        {name:"max 3"},
-        { $set:{name:"max 4" , price: 500} }
-    )
-    console.log(data);
-}
-// updateInDB();
-
-const deleteInDB = async ()=>{
-    const ProductModel = mongoose.model('products' , ProductSchema);
-    let data = await ProductModel.deleteOne({name: 'max 4'});
-    console.log(data);
-}
-// deleteInDB();
-
-const findInDB = async ()=>{
-    const ProductModel = mongoose.model('products' , ProductSchema);
-    let data = await ProductModel.find({name :'max 4'});
-    console.log(data);
-}
-
-findInDB();
+app.listen(5000);
