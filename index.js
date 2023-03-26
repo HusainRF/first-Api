6d7acbc95320b5a300bc5acf0cdb4c6479f33bce
HusainRF <justgetupanddoit9@gmail.com>
2023-03-26 23:42:48 +0530

    used multer module to upload or download file

--- index.js
+++ index.js
@@ -1,36 +1,21 @@
 const express = require("express");
-const product = require("./product");
-require('./config');
-const Product = require('./product');
+const multer = require("multer");
 const app = express();
 
-app.use(express.json());
+const upload = multer({
+    storage: multer.diskStorage({
+        destination: function(req ,file ,cb){
+            cb(null , 'uploads');
+        },
+        filename:function (req, file , cb){
+            cb(null , file.fieldname + "-" + Date.now() + ".jpeg")
+        }
+    })
+}).single('user_file');
 
-app.post("/create", async (req, resp)=>{
-    let data = new Product(req.body);
-    let result = await data.save();
-    resp.send(result);
+app.post("/", upload , async (req, resp)=>{
+    resp.send("file uploaded");    
 });
-
-app.get("/list" , async (req ,resp)=>{
-    let data = await Product.find();
-    resp.send(data);
-})
-
-app.delete("/delete/:_id" , async (req ,resp)=>{
-    console.log(req.params);
-    const data = await Product.deleteOne(req.params);
-    resp.send(data);
-})
-
-app.put("/update/:_id" , async (req, resp) => {
-    console.log(req.params);
-    const data = await Product.updateOne(
-        req.params,
-        {$set : req.body}
-    )
-    resp.send(data);
-})
 
 
 app.listen(5000);
